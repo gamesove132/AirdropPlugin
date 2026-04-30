7f3161a1bf8987cee91507da7b68ae49ec3c165c
gamesove132 <gamesove132@gmail.com>
2026-04-30 20:10:20 +0300

    Update AirdropPlugin.java

--- src/main/java/com/airdrop/AirdropPlugin.java
+++ src/main/java/com/airdrop/AirdropPlugin.java
@@ -10,31 +10,25 @@ public class AirdropPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Створюємо конфігурацію за замовчуванням
         saveDefaultConfig();
 
-        // Ініціалізуємо менеджер[cite: 3, 5]
         this.airdropManager = new AirdropManager(this);
 
-        // Реєструємо команду
         AirdropCommand cmd = new AirdropCommand(this);
         
-        // Встановлюємо лише Executor (обробник команди)
         if (getCommand("airdrops") != null) {
             getCommand("airdrops").setExecutor(cmd);
-            // РЯДОК З setTabCompleter ВИДАЛЕНО, щоб не було помилки[cite: 5]
         }
 
-        getLogger().info("AirdropPlugin активовано успішно!");[cite: 5]
+        getLogger().info("AirdropPlugin activated successfully!");
     }
 
     @Override
     public void onDisable() {
-        // Зупиняємо всі активні айрдропи перед вимкненням[cite: 3]
         if (airdropManager != null) {
-            airdropManager.getActiveAirdrops().values().forEach(event -> event.cancel());[cite: 3, 9]
+            airdropManager.getActiveAirdrops().values().forEach(event -> event.cancel());
         }
-        getLogger().info("AirdropPlugin вимкнено.");[cite: 5]
+        getLogger().info("AirdropPlugin disabled.");
     }
 
     public AirdropManager getAirdropManager() {
